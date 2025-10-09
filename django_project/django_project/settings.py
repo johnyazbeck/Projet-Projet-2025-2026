741d037e5e61a2221fb809a30bcbb08f6fc38f99
oziylan <osman.ziylan@helb-prigogine.be>
2025-10-09 12:55:46 +0200

    Login
    On peut s'inscrire et se connecter mais on ne sais pas encore se déconnecter
    
    Corey Chauffeur travaille avec sqllite faut trouver une solution pour passer à
    maria db

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -129,3 +129,4 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+LOGOUT_REDIRECT_URL = 'login'
--- django_project/users/signals.py
+++ django_project/users/signals.py
@@ -3,13 +3,18 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile
 
+#from stackoverflow https://stackoverflow.com/questions/63962443/create-a-post-save-signal-that-creates-a-profile-object-for-me
 
+# Signal to create a profile when creating a user
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
+        # Create a profile for the user
         Profile.objects.create(user=instance)
 
-
+# Signal to save the profile of the user
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
-    instance.profile.save()
+    # user has a profil? save
+    if hasattr(instance, 'profile'):  # profile exist?
+        instance.profile.save()
